fd9d7e084d4f1199011dc06f6093fe4ec8205f50
Felix Horn <flx.horn@gmail.com>
2020-11-17 14:15:10 +0100

    Add addListItem with normalized data

--- src/App.js
+++ src/App.js
@@ -5,38 +5,117 @@ import InputForm from './01.UI-Elements/InputForm'
 import ListItem from './01.UI-Elements/ListItem'
 
 function App() {
-  const [list, setList] = useState([])
+  const [list, setList] = useState({
+    byId: {
+      '65afds4': {
+        id: '65afds4',
+        titleListItem: 'Milk',
+        isCheckmarked: false,
+      },
+      a84f55d: {
+        id: 'a84f55d',
+        titleListItem: 'Butter',
+        isCheckmarked: true,
+      },
+    },
+    allIds: ['65afds4', 'a84f55d'],
+  })
+
+  // const [list, setList] = useState({
+  //   byId: {},
+  //   allIds: [],
+  // })
 
   return (
     <div className="App">
       <InputForm onCreateListItem={addListItem} />
-      {list.map(({ titleListItem, isCheckmarked, id }, index) => (
-        <ListItem
-          onCheckboxClick={() => toggleListItem(index)}
-          onDeleteClick={() => deleteListItem(id)}
-          titleListItem={titleListItem}
-          isCheckmarked={isCheckmarked}
-          key={id}
-        />
-      ))}
+      {list.allIds.map((id) => {
+        const { titleListItem, isCheckmarked } = list.byId[id]
+        return (
+          <ListItem
+            key={id}
+            titleListItem={titleListItem}
+            isCheckmarked={isCheckmarked}
+            onCheckboxClick={() => toggleListItem(id)}
+            onDeleteClick={() => deleteListItem(id)}
+          />
+        )
+      })}
     </div>
   )
 
   function addListItem(titleListItem) {
-    setList([
+    console.log(titleListItem)
+
+    const generatedId = uuid()
+
+    setList({
+      byId: {
+        ...list.byId,
+        [generatedId]: {
+          id: generatedId,
+          titleListItem: titleListItem,
+          isCheckmarked: false,
+        },
+      },
+      allIds: [...list.allIds, generatedId],
+    })
+  }
+
+  /* [
       ...list,
       { titleListItem: titleListItem, isCheckmarked: false, id: uuid() },
     ])
-  }
+  } */
 
-  function toggleListItem(index) {
-    // setList(list[index].isCheckmarked = !list[index].isCheckmarked)
-    const item = list[index]
-    setList([
-      ...list.slice(0, index),
-      { ...item, isCheckmarked: !item.isCheckmarked },
-      ...list.slice(index + 1),
-    ])
+  /* 
+  list = [
+
+    { titleListItem: 1, isCheckmarked: false, id: 65afds4 }
+    { titleListItem: 2, isCheckmarked: true, id: a6ew4af }
+    { titleListItem: 3, isCheckmarked: false, id: adfsv49 }
+
+  ]
+  */
+
+  /* 
+  list = {
+
+    byId : {
+      "65afds4" : {
+        id: "65afds4",
+        titleListItem : "Milk",
+        isCheckmarked: false,
+      },
+      "a84f55d" : {
+        id: "a84f55d",
+        titleListItem : "Butter",
+        isCheckmarked: true,
+      },
+    },
+    allIds : ["65afds4","a84f55d"] 
+
+  }
+  */
+
+  function toggleListItem(id, index) {
+    setList(list.map((item) => item.id === id))
+    setList((list.id = id.isCheckmarked = !list[id].isCheckmarked))
+    // const item = list[index]
+    // setList([
+    //   ...list.slice(0, index),
+    //   { ...item, isCheckmarked: !item.isCheckmarked },
+    //   ...list.slice(index + 1),
+    // ])
+
+    console.log(list[id].isCheckmarked)
+
+    console.log(
+      index,
+      list[index],
+      list.map((item) => item.titleListItem),
+      list.map((_, index) => index)
+    )
   }
 
   function deleteListItem(id) {
