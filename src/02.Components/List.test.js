f973aeaed82e155f93d87bf3cc7bf22bafddf5e9
Felix Horn <flx.horn@gmail.com>
2020-11-21 09:58:49 +0100

    Update List component test

--- src/02.Components/List.test.js
+++ src/02.Components/List.test.js
@@ -5,6 +5,7 @@ describe('List', () => {
   it('renders the list correctly', () => {
     const { container } = render(
       <List
+        listAllocation={['a', 'b']}
         list={{
           allIds: ['a', 'b', 'c'],
           byId: {
@@ -28,5 +29,57 @@ describe('List', () => {
       />
     )
     expect(container.firstChild).toMatchSnapshot()
+  })
+  it('only renders unchecked listItems', () => {
+    const { queryByText } = render(
+      <List
+        listAllocation={['a']}
+        list={{
+          allIds: ['a', 'b'],
+          byId: {
+            a: {
+              id: 'a',
+              title: 'Milk',
+              isChecked: false,
+            },
+            b: {
+              id: 'b',
+              title: 'Cheese',
+              isChecked: true,
+            },
+          },
+        }}
+      />
+    )
+    const milkListItem = queryByText(/milk/i)
+    const cheeseListItem = queryByText(/cheese/i)
+    expect(milkListItem).toBeInTheDocument()
+    expect(cheeseListItem).not.toBeInTheDocument()
+  })
+  it('only renders checked listItems', () => {
+    const { queryByText } = render(
+      <List
+        listAllocation={['b']}
+        list={{
+          allIds: ['a', 'b'],
+          byId: {
+            a: {
+              id: 'a',
+              title: 'Milk',
+              isChecked: false,
+            },
+            b: {
+              id: 'b',
+              title: 'Cheese',
+              isChecked: true,
+            },
+          },
+        }}
+      />
+    )
+    const milkListItem = queryByText(/milk/i)
+    const cheeseListItem = queryByText(/cheese/i)
+    expect(milkListItem).not.toBeInTheDocument()
+    expect(cheeseListItem).toBeInTheDocument()
   })
 })
