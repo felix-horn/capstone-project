4d62100fc48e585321a5d89354ab8a6b1a32b9a2
Felix Horn <flx.horn@gmail.com>
2020-12-10 08:13:00 +0100

    Add quagga barcode scanner and app shortcut

--- src/03.ScannerPage/ScannerPage.js
+++ src/03.ScannerPage/ScannerPage.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import styled from 'styled-components/macro'
 import Header from '../00.SharedComponents/01.UI-Elements/02.Components/Header'
 import MenuWrapped from '../00.SharedComponents/01.UI-Elements/02.Components/MenuWrapped'
+import Scanner from './02.Components/Scanner'
 
 ScannerPage.propTypes = {
   // database: PropTypes.object.isRequired,
@@ -12,15 +13,27 @@ ScannerPage.propTypes = {
 
 export default function ScannerPage() {
   const [isMenuVisible, setIsMenuVisible] = useState(false)
+  const [camera, setCamera] = useState(false)
+  const [result, setResult] = useState(null)
   return (
     <>
       <HeaderStyled onClick={toggleMenu} />
       {isMenuVisible && <MenuWrapped toggleMenu={toggleMenu} />}
       <ShopTitleStlyed>TEST</ShopTitleStlyed>
+      <ScannerWrapper>
+        <p>{result ? result : 'Scanning...'}</p>
+        <Container>{camera && <Scanner onDetected={onDetected} />}</Container>
+        <button onClick={() => setCamera(!camera)}>
+          {camera ? 'Stop' : 'Start'}
+        </button>
+      </ScannerWrapper>
     </>
   )
   function toggleMenu() {
     setIsMenuVisible(!isMenuVisible)
+  }
+  function onDetected(result) {
+    setResult(result)
   }
 }
 
@@ -33,4 +46,30 @@ const HeaderStyled = styled(Header)`
 `
 const ShopTitleStlyed = styled.h1`
   margin-top: 40px !important;
+`
+const ScannerWrapper = styled.div`
+  text-align: center;
+  height: 70vh;
+  width: 70vw;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`
+const Container = styled.div`
+  position: relative;
+
+  #interactive.viewport canvas,
+  video {
+    width: 400px;
+    height: 300px;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  #interactive.viewport canvas.drawingBuffer,
+  video.drawingBuffer {
+    width: 400px;
+    height: 300px;
+  }
 `
