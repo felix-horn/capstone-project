e777cb52bd28b539a9a1190c1babe75071150db8
felix <flx.horn@gmail.com>
2021-01-16 20:33:40 +0100

    Update CheckedItemSummary.test.js

--- src/01.Overview/01.UI-Elements/CheckedItemsSummary.test.js
+++ src/01.Overview/01.UI-Elements/CheckedItemsSummary.test.js
@@ -3,17 +3,19 @@ import CheckedItemsSummary from './CheckedItemsSummary'
 
 describe('CheckedItemsSummary', () => {
   it('shows the correct summary for 0 checked items', () => {
-    const { queryByText } = render(<CheckedItemsSummary lengthCheckedIds={0} />)
+    const { queryByText } = render(
+      <CheckedItemsSummary quantityCheckedIds={0} />
+    )
     const summaryText = queryByText('und')
     expect(summaryText).not.toBeInTheDocument()
   })
   it('shows the correct summary for 1 checked items', () => {
-    const { getByText } = render(<CheckedItemsSummary lengthCheckedIds={1} />)
+    const { getByText } = render(<CheckedItemsSummary quantityCheckedIds={1} />)
     const summaryText = getByText('und 1 abgehakter Eintrag')
     expect(summaryText).toBeInTheDocument()
   })
   it('shows the correct summary for 2 checked items', () => {
-    const { getByText } = render(<CheckedItemsSummary lengthCheckedIds={2} />)
+    const { getByText } = render(<CheckedItemsSummary quantityCheckedIds={2} />)
     const summaryText = getByText('und 2 abgehakte Einträge')
     expect(summaryText).toBeInTheDocument()
   })
